ca6f016561106bc53c7bc87ae0ea4b7daa73b510
Narzana <narzana@mail.ru>
2023-06-29 17:53:48 +0300

    Add files via upload
    Fixed task 4

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,11 +3,10 @@ function parseProducts(json) {
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
-    // в объект с помощью функции JSON.parse(json)
-    // и запишите в переменную data
+    // в объект с помощью функции JSON.parse(json) и запишите в переменную data
     let data = JSON.parse(json);
-    return data[products];
     // Верните как результат функции свойство products объекта data
+    return data.products;
 }
 
 // Напишите функцию renderProductsCards(json)
@@ -21,10 +20,12 @@ function renderProductsCards(json) {
     // Запишите в переменную length значение свойства products.length
     let length = products.length;
     // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-    for (let i=0; i < length-1; i++)
+    // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+    for (let i=0; i < length; i++)
     {
         addProduct(products[i]);
     }
-    // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+    
+    
 }
 
